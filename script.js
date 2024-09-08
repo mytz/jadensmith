b94f5cd288afb994ee127daf01a9965a461bd86a
mytz <98504002+mytz@users.noreply.github.com>
2024-09-08 02:33:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const songs = ['Charli_xcx_-_Girl_so_confusing.mp3', 'Blue_Foundation_-_Eyes_On_Fire.mp3'];
     let songIndex = 0;
 
-    // Inicializar la primera canción y mostrar el botón de play
     function updateSong() {
         audioPlayer.src = songs[songIndex];
         songTitle.textContent = songs[songIndex];
@@ -57,8 +56,8 @@ document.addEventListener('DOMContentLoaded', function() {
         playPauseButton.classList.add('fa-pause');
     }
 
-    // Manejar Play/Pause
     playPauseButton.addEventListener('click', function() {
+        console.log('Play/Pause button clicked');
         if (audioPlayer.paused) {
             audioPlayer.play();
             playPauseButton.classList.remove('fa-play');
@@ -70,19 +69,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Manejar Forward
     forwardButton.addEventListener('click', function() {
+        console.log('Forward button clicked');
         songIndex = (songIndex + 1) % songs.length;
         updateSong();
     });
 
-    // Manejar Backward
     backwardButton.addEventListener('click', function() {
+        console.log('Backward button clicked');
         songIndex = (songIndex - 1 + songs.length) % songs.length;
         updateSong();
     });
 
-    // Inicializar la primera canción
     playPauseButton.classList.add('fa-play'); // Asegura que el botón de play esté visible al inicio
     updateSong();
 });
